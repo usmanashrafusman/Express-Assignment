66d69b72bcc32129a3e66e89a77caadec0e0e990
Muhammad Usman <37510976+usmanashrafusman@users.noreply.github.com>
2021-12-25 01:25:38 +0500

    Add files via upload
    index

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port =  process.env.PORT || 3000;
 const path = require("path");
 let ejs = require('ejs');
 
-// app.use(express.static(path.join(__dirname, "public")));
 
 
 
